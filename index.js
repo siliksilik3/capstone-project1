c7be70639651ebb57026ad1fc410981c16e99734
Alex <silibitkins.aleksandrs@gmail.com>
2024-10-24 16:08:52 +0300

    Complete registration function and start login

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 let posts=[];
 let i=0;
- 
+let clients=[];
+let j=0; //client id
 
 
 
@@ -24,6 +25,13 @@ app.get("/", (req, res) => {
 
 app.get("/create", (req, res) => {  
   res.render("partials/create.ejs");
+});
+
+app.get("/registration", (req, res)=>{
+  res.render("partials/registration.ejs");
+});
+app.get("/login", (req, res)=>{
+  res.render("partials/login.ejs");
 });
 
 app.get("/view/:id", (req, res) => {  
@@ -55,15 +63,27 @@ app.post("/", (req, res) => {
     const {title, body}=req.body;
     const id= i;
     if(!req.body.title){
-      
+      res.redirect("/create");
     }else{
       posts.push({id, title, body});
       res.redirect("/");
       i++;
     }
-  
-  
 });
+app.post("/registration", (req, res)=>{
+  const {login, password}=req.body;
+  const clientId=j;
+  if(!req.body.login || !req.body.password){
+    res.redirect("/registration");
+  }else{
+clients.push({clientId, login, password});
+res.redirect("/");
+j++;
+console.log(clients);
+
+  }
+});
+
 app.post('/delete/:id', (req, res) => {
    posts = posts.filter(p => p.id != req.params.id);
   res.redirect('/');
